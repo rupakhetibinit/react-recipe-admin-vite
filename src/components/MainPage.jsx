bb860d75bcb63dc569918f42fa7dcca16f8946b6
Binit <cruiserponder@protonmail.com>
2022-04-23 19:43:24 +0545

    more bugs with fixes

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -6,33 +6,12 @@ const ShowUsers = React.lazy(() => import('./ShowUsers'));
 import SideBar from './SideBar';
 
 const MainPage = () => {
-	// const { user, setUser } = useContext(AuthContext);
 	const [selectedTab, setSelectedTab] = useState('Orders');
-	// const tabs = [
-	//   { title: 'Dashboard', content: HomePage },
-	//   { title: 'Orders', content: ShowOrders },
-	//   { title: 'Users', content: ShowUsers },
-	//   { title: 'Add Recipe', content: AddRecipe },
-	// ];
 
 	return (
 		<Flex flexDirection='row'>
-			{/* <Button
-        onClick={() => {
-          setUser({
-            isAdmin: false,
-            email: '',
-            token: '',
-            name: '',
-          });
-          localStorage.removeItem('user');
-        }}
-      >
-        Log Out
-      </Button> */}
 			<SideBar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
 			<Flex alignItems='center' justifyContent='center' marginLeft={20}>
-				{/* {selectedTab === 'Dashboard' && <HomePage />} */}
 				<Suspense fallback={<Spinner />}>
 					{selectedTab === 'Orders' && <ShowOrders />}
 					{selectedTab === 'Add Recipe' && <AddRecipe />}
